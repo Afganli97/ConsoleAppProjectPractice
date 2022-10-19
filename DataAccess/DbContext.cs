6665dd5fe7c48a84ef406c9ae5c9de03dc963727
Afganli97 <113414023+Afganli97@users.noreply.github.com>
2022-10-20 02:31:23 +0400

    Create DataBase

---
+++ DataAccess/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess
+{
+    public static class DbContext
+    {
+        public static List<Project> projects;
+        public static List<Developer> developers;
+        static DbContext()
+        {
+            projects = new List<Project>();
+            developers = new List<Developer>();
+        }
+    }
+}
--- Entities/Models/Project.cs
+++ Entities/Models/Project.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Entities.Models
 {
-    internal class Project : IEntity
+    public class Project : IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
